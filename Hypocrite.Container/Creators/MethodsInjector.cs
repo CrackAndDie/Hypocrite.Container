04c5ccd9601854b3f984ffb18ce4251a491d8f9a
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2024-12-19 13:09:10 +0300

    GenerateFactoryWithParams for methods added

--- Hypocrite.Container/Creators/MethodsInjector.cs
+++ Hypocrite.Container/Creators/MethodsInjector.cs
@@ -1,4 +1,9 @@
-﻿namespace Hypocrite.Container.Creators
+﻿using Hypocrite.Container.Extensions;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Hypocrite.Container.Creators
 {
     internal static class MethodsInjector
     {
@@ -13,10 +18,64 @@
             }
             else
             {
-                injector = GenerateFactoryWithParams<T>(args.Keys.ToArray());
+                injector = GenerateFactoryWithParams<T>(args);
                 _cachedWithParams.Add(typeof(T), injector);
             }
             injector.Invoke(instance, args);
+        }
+
+        private static Action<object, Dictionary<string, object[]>> GenerateFactoryWithParams<T>(Dictionary<string, object[]> args)
+        {
+            // Params
+            var instanceParam = Expression.Parameter(typeof(object), "instance");
+            // Convert to required type
+            var typedInstance = Expression.Convert(instanceParam, typeof(T));
+            // Param for args 
+            var dictParam = Expression.Parameter(typeof(Dictionary<string, object[]>), "args");
+
+            var list = new List<Expression>();
+            // methods shite
+            var methodInfos = typeof(T).GetTypeInfo().DeclaredMethods;
+            foreach (var methodInfo in methodInfos)
+            {
+                if (!args.ContainsKey(methodInfo.Name))
+                    continue;
+
+                Expression call = Expression.Call(
+                                  typeof(DictionaryExtensions),
+                                  "GetValue",
+                                  new[] { typeof(object[]) },
+                                  new Expression[]
+                                   {
+                                        dictParam,
+                                        Expression.Constant(methodInfo.Name)
+                                   });
+
+                // Convert params to args
+                var methodArgs = methodInfo.GetParameters()
+                    .Select((p, i) =>
+                        Expression.Convert(
+                            Expression.ArrayIndex(call, Expression.Constant(i)),
+                            p.ParameterType))
+                    .ToArray();
+
+                List<Expression> fullMethodArgs = new List<Expression>() { typedInstance };
+                fullMethodArgs.AddRange(methodArgs);
+
+                Expression methodCall = Expression.Call(
+                                  typeof(T),
+                                  methodInfo.Name,
+                                  null,
+                                  fullMethodArgs.ToArray());
+                list.Add(methodCall);
+            }
+
+            // Block expression for inits
+            var blockExpr = Expression.Block(list);
+
+            // Compiling the lambda
+            var lambda = Expression.Lambda<Action<object, Dictionary<string, object[]>>>(blockExpr, instanceParam, dictParam);
+            return lambda.Compile();
         }
     }
 }
