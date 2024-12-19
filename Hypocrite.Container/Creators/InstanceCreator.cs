a2b46a79c496d71d96182c8f6900638ce7a3e03f
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2024-12-19 14:59:56 +0300

    REMOVED Creator without params!!!

--- Hypocrite.Container/Creators/InstanceCreator.cs
+++ Hypocrite.Container/Creators/InstanceCreator.cs
@@ -43,51 +43,5 @@ namespace Hypocrite.Container.Creators
             var lambda = Expression.Lambda<Func<object[], object>>(newExpr, param);
             return lambda.Compile();
         }
-
-        private static readonly Dictionary<Type, Func<object>> _cachedNoParams = new Dictionary<Type, Func<object>>();
-        internal static T CreateNoParams<T>() where T : new()
-        {
-            Func<object> creator;
-            // check for cache
-            if (_cachedNoParams.TryGetValue(typeof(T), out var cachedCreator))
-            {
-                creator = cachedCreator;
-            }
-            else
-            {
-                creator = GenerateFactoryNoParams<T>();
-                _cachedNoParams.Add(typeof(T), creator);
-            }
-            return (T)creator.Invoke();
-        }
-
-        private static Func<object> GenerateFactoryNoParams<T>() where T : new()
-        {
-            Expression<Func<T>> expr = () => new T();
-            NewExpression newExpr = (NewExpression)expr.Body;
-
-            var method = new DynamicMethod(
-                name: "lambda",
-                returnType: typeof(object),
-                parameterTypes: new Type[0],
-                m: typeof(InstanceCreator).Module,
-                skipVisibility: true);
-
-            ILGenerator ilGen = method.GetILGenerator();
-            // Constructor for value types could be null
-            if (newExpr.Constructor != null)
-            {
-                ilGen.Emit(OpCodes.Newobj, newExpr.Constructor);
-            }
-            else
-            {
-                LocalBuilder temp = ilGen.DeclareLocal(newExpr.Type);
-                ilGen.Emit(OpCodes.Ldloca, temp);
-                ilGen.Emit(OpCodes.Initobj, newExpr.Type);
-                ilGen.Emit(OpCodes.Ldloc, temp);
-            }
-            ilGen.Emit(OpCodes.Ret);
-            return (Func<object>)method.CreateDelegate(typeof(Func<object>));
-        }
     }
 }
